f6426b68248adbaa6787505112024d93aa6b9c05
4lch4 <git@4lch4.dev>
2021-03-20 15:44:28 -0500

    fix(tests): fixed the command test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
 import cmd = require('../src')
 
@@ -6,14 +6,7 @@ describe('@4lch4/git-finder', () => {
   test
   .stdout()
   .do(() => cmd.run([]))
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .do(() => cmd.run(['--name', 'jeff']))
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .it('creates a local report', ctx => {
+    expect(ctx.stdout).to.contain('│ git-finder │')
   })
 })
